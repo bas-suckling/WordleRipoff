d4933e9dd4712a3014ce825f77a184536b0bf47c
Bas <suckling.w.s@gmail.com>
2024-02-25 16:01:21 +1300

    add game logic

--- game.py
+++ game.py
@@ -8,13 +8,11 @@ def generateWordList():
             currentline = line.split(",")
             for word in currentline:
                 if word != '\n':
-                    wordList.append(word)
+                    wordList.append(word.lower())
     return wordList
 
 def getNewWord(wordList): 
     return random.choice(wordList)
-
-
 
 misplacedLetters = list()
 incorrectLetters = list()
@@ -23,21 +21,54 @@ currentGuess = 0
 gameTitle = 'My Wordle Ripoff to Learn Python and RaspberryPi'
 
 words = generateWordList()
-
+solution = getNewWord(words)
+boardState = ["?", "?", "?", "?", "?"]
+print (solution)
+print('--------------------------------------------------')
 print('Welcome to ', gameTitle)
 print('Your object is to figure out the 5 letter word.')
+print('--------------------------------------------------')
 
 while currentGuess < maximumGuesses :
     print('You have ', maximumGuesses - currentGuess, ' attempt(s) left.')
-    userInput = input("What is your guess? ")
+    print("What is your guess?")
+    print("")
+    userInput = input("")
     lcUserInput = userInput.lower()
-    if (not lcUserInput.isalpha()) or (len(lcUserInput) != 5):
-        print('Invalid input')
+    if not lcUserInput.isalpha():
+        print('Invalid input, must only contain letters')
+        print('====')
+    elif len(lcUserInput) != 5: 
+        print('Invalid input, must be a 5 letter word')
+        print('====')
     else:
-        print ('Game logic goes here')
+        for index, letter in enumerate(lcUserInput):
+            if letter not in solution:
+                incorrectLetters.append(letter)
+            elif letter != solution[index]:
+                misplacedLetters.append(letter)
+            else: 
+                boardState[index] = letter
+                misplacedLetters = list(filter(lambda x: x!=letter, misplacedLetters))
+        print("".join(boardState))
+        
+    if (lcUserInput == solution):
+        print("Congratulations you got it!")
+        break
+
+    else:
+        print('')
+        print('misplaced letters: ', ", ".join(set(misplacedLetters)))
+        print('incorrect letters: ', ", ".join(set(incorrectLetters)))
+        print('')
+        print('====')
+        print('')
         currentGuess += 1
 
-
+if (lcUserInput != solution): 
+    print('')
+    print('The word was ', solution)
+    print('You lose, better luck next time.')
 
 
 
